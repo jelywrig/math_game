6cc897c4ca6bc96ae73bf32403b7067c0222f962
Jessie Wright <jessie.wright@gmail.com>
2020-05-25 19:55:59 +0000

    implemented Question

---
+++ Question.rb
@@ -0,0 +1,29 @@
+class Question
+
+  Operators = ['plus', 'minus', 'divided by (int math)', 'times']
+  def initialize()
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    operator_selector = rand(0..3)
+    @operator_text = Operators[operator_selector]
+    case operator_selector
+      when 0
+        @answer = @num1 + @num2
+      when 1
+        @answer = @num1 - @num2
+      when 2
+        @answer = @num1 / @num2
+      when 3
+        @answer = @num1 * @num2
+    end
+  end
+
+  def to_s
+    "What does #{@num1} #{@operator_text} #{@num2} equal?"
+  end
+
+  def check_answer(answer)
+    answer == @answer
+  end
+  
+end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 load './Player.rb'
+load './Question.rb'
 
 player1 = Player.new("Jessie")
 
@@ -7,3 +8,10 @@ puts player1.name, player1.lives
 player1.lose_life
 
 puts player1.lives
+
+q1 = Question.new
+
+puts q1
+answer = gets.chomp.to_i
+
+puts q1.check_answer(answer)
